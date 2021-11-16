78dd232d186be7196e62e897e77be6a883f3e4f5
VERHILLE Arnaud <gist974@gmail.com>
2021-11-16 21:08:42 +0400

    Fixing Pendule Bifurcation

--- 3.Pendule/PenduleBifurcation.py
+++ 3.Pendule/PenduleBifurcation.py
@@ -11,37 +11,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import solve_ivp
 
-# ----------
-# CONSTANTES 
+omega = 2*np.pi      #
+omega0 = 1.5*omega   # paramètres proposés par Taylor
+beta = omega0/4      #
 
-OMEGA = 2*np.pi
-OMEGA0 = 1.5*OMEGA
-BETA = OMEGA0/4
-GAMMA = 1.06
-
-# ---------
-# FONCTIONS
-
-def fun(t,x):
-    phi,phipoint = x[0], x[1]
+def fun(t, x):
+    phi, phipoint = x[0], x[1]
     v = phipoint
-    a = -2*BETA*phipoint - OMEGA0**2*np.sin(phi) + GAMMA*OMEGA0**2*np.cos(OMEGA*t)
+    a = -2*beta*phipoint - omega0**2*np.sin(phi) + gamma*omega0**2*np.cos(omega*t)
     return v, a
 
-t_max = 61
-N_pas = 1000
-N_points = 10
-t= np.arange(t_max - N_points, t_max)
-liste_gamma = np.arange (1.0,1.1,0.0002)
-thetas, gammas = [],[]
+t_max = 61          # fin de l'intégration
+N_pas = 1000        # nb de pas
+N_points = 10       # nb de points affichés à chaque valeur de gamma
+t = np.arange(t_max - N_points, t_max)
+liste_gamma = np.arange(1.0, 1.1, 0.0002)
+thetas, gammas = [], []
 y0=[0,0]
-for i, gamma in enumerate(liste_gamma) :
-    sol = solve_ivp(fun, [0,t_max],y0, t_eval=np.linspace(0., t_max, N_pas), rtol=1e-9, dense_output=True)
-    gammas+= [gamma]* N_points
+for i, gamma in enumerate(liste_gamma):
+    sol = solve_ivp(fun, [0, t_max], y0, t_eval=np.linspace(0., t_max, N_pas), rtol=1e-9, dense_output=True)
+    gammas += [gamma] * N_points
     thetas += list((sol.sol(t)[0]+np.pi)%(2*np.pi)-np.pi)
-    print("gamma=%.6f [%d/%d]" % (gamma,i+1, len(liste_gamma)), end="\r")
-    y0 = sol.sol(t_max)  # initialisation pour le prochain calcul
-
-plt.scatter(gammas,thetas,s=1,c='black')
-plt.ylim(-1.5,0)
+    print("gamma=%.6f [%d/%d]" % (gamma, i+1, len(liste_gamma)), end="\r")
+    y0=sol.sol(t_max)  # initialisation pour le prochain calcul
+plt.scatter(gammas, thetas, c='black', s=1)
+plt.ylim(-1.5, 0)
 plt.show()
